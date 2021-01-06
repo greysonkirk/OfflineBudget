8910256ba76d09241bea7014fb06e20b0630f027
Greyson Kirk <greysonkirk@yahoo.com>
2021-01-06 06:40:25 -0600

    commit

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,12 +1,10 @@
 const FILES_TO_CACHE = [
     "/",
     "/index.html",
-    "/style.css",
+    "style.css",
     "/index.js",
     "/icons/icon-192x192.png",
     "/icons/icon-512x512.png"
-
-
 ];
 
 
